b27e7f5b16063110c695abb422170453ffe7669a
Marco Krage <marco@my-azur.de>
2018-05-05 14:42:34 +0200

    domain filter

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ PHP caching proxy between tile server and your map application. To save bandwith
 ## usage
 Change variables in index.php and set RewriteBase in .htaccess.
 
-Use `https://yourdomain.com/appfolder/{zoom}/{x}/{y}.png` as tile url and it serves a cached tile from e.g. `http://c.tile.openstreetmap.org/{zoom}/{x}/{y}.png`
+Use `https://yourdomain.com/appfolder/https://tileserver.com/{zoom}/{x}/{y}.png` as tile url and it serves a cached tile from e.g. `http://c.tile.openstreetmap.org/{zoom}/{x}/{y}.png`
 
-Add other tile server by extending the $maptypes array.
+Add other tile server by extending the $domains array.
 
 ### leaflet Example
 ```
-var tile_osm = L.tileLayer('https://yourdomain.com/maps/tile-cache/osm/{z}/{x}/{y}.png', {
+var tile_osm = L.tileLayer('https://yourdomain.com/appfolder/https://http://{s}.tile.openstreetmap.org/{zoom}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 ```
--- index.php
+++ index.php
@@ -1,148 +1,85 @@
 <?php
 
-$baseDir = "maps/tile-cache"; // http://example.com/$baseDir/5/54/10.png; eg. sub/folder
 $cacheDir = "cache";              // http://example.com/$baseDir/$cacheDir/
 $errorTile = "./tile-error.png";  // relative to $baseDir
 $logging = false;
 
 $cacheHeaderTime = 60*60*24*365; // Browser Header (sec)
-$cacheFileTime = 60*60*24*365/4; // max File Age (sec)
+$cacheFileTime = 60*60*24*356 / 4; // max File Age (sec)
 
-$maptypes = array();
+$domains = array(
+	"a.tile.openstreetmap.org" => "osm",
+	"b.tile.openstreetmap.org" => "osm",
+	"c.tile.openstreetmap.org" => "osm",
+	"secais.dfs.de" => "icao"
+);
 
-$maptypes['osm']['url'] = "http://{s}.tile.osm.org/{z}/{x}/{y}";
-$maptypes['osm']['subdomains'] = "abc";
-$maptypes['stamen-watercolor']['url'] = "http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}";
-$maptypes['stamen-watercolor']['subdomains'] = "abcd";
+$tileURL = str_replace(str_replace(basename(__FILE__), '', $_SERVER['SCRIPT_NAME']), "", $_SERVER['REQUEST_URI']);
+$tileURLParts = parse_url($tileURL);
+$tileDomain = $tileURLParts['host'];
 
-/* +++++++++++++++++++++++++++++++++ */
+$cacheDir = $cacheDir."/".$domains[$tileDomain];
+$cacheTileFile = $cacheDir."/".substr($tileURLParts['path'], 1);
 
-$requestURI = $_SERVER['REQUEST_URI'];
-
-if(substr($requestURI, 0, 1) == "/") {
-  $requestURI = substr($requestURI, 1);
+if(!array_key_exists($tileDomain, $domains)) {
+	die('Domain not allowed');
 }
 
-$requestURI = str_replace($baseDir."/", "", $requestURI);
-list($maptype, $zoom, $x, $y) = explode("/", $requestURI);
+$cacheRes = true;
 
-if($maptype == "" || $zoom == "" || $x == "" || $y == "") {
-  var_dump($maptype);
-  var_dump($zoom);
-  var_dump($x);
-  var_dump($y);
-  die("Nicht alle Parameter angegeben: /{maptype}/{zoom}/{x}/{y}.png");
+if(!file_exists($cacheTileFile)) {
+  logger($cacheTileFile." new tile ".$tileURL);
+  $cacheRes = cacheTile($tileURL, $cacheTileFile);
+}else{
+  $fileAge = time()-filemtime($cacheTileFile);
+  logger($cacheTileFile." existing tile ".$fileAge."/".$cacheFileTime);
+  if($fileAge > $cacheFileTime) {
+    logger($cacheTileFile." renew tile");
+    $cacheRes = cacheTile($tileURL, $cacheTileFile);
+  }
 }
 
-if(!array_key_exists($maptype, $maptypes)) {
-  var_dump($maptype);
-  die("Unbekannter Maptype");
+if(!$cacheRes) {
+  $cacheTileFile = $errorTile;
 }
 
-$tileUrl = $maptypes[$maptype]['url']; 
-$tileHostSubdomains = $maptypes[$maptype]['subdomains']; 
-
-$tileUrl = str_replace("{z}", $zoom, $tileUrl);
-$tileUrl = str_replace("{x}", $x, $tileUrl);
-$tileUrl = str_replace("{y}", $y, $tileUrl);
-
-if($tileHostSubdomains) {
-  $tileHostSubdomain = $tileHostSubdomains[rand(0, strlen($tileHostSubdomains)-1)];
-  $tileUrl = str_replace("{s}", $tileHostSubdomain, $tileUrl);
-}
-
-$cacheDir = $cacheDir."/".$maptype;
-$cacheTileFile = $cacheDir."/".$zoom."/".$x."/".$y;
-
-if(!$imgData = cacheTile($tileUrl, $cacheTileFile)) {
-  logger($tileUrl." using error Tile");
-  $imgData = file_get_contents($errorTile);
-}
+$imgData = file_get_contents($cacheTileFile);
 
 $file_extension = strtolower(substr(strrchr(basename($cacheTileFile),"."),1));
-
 switch( $file_extension ) {
-  case "png": $mimetype="image/png"; break;
-  case "gif": $mimetype="image/gif"; break;
-  case "jpeg":
-  case "jpg": $mimetype="image/jpg"; break;
-  default:
+    case "png": $mimetype="image/png"; break;
+    case "gif": $mimetype="image/gif"; break;
+    case "jpeg":
+    case "jpg": $mimetype="image/jpg"; break;
+    default:
 }
 
-
-//set last-modified header
-// TODO: make $imgData->data and $imgData->lastModified
-#header("Last-Modified: ".gmdate("D, d M Y H:i:s", $lastModified)." GMT");
-
-//set expires header
-header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + ($cacheHeaderTime)));
-
-//set etag-header
-header("Etag: ".md5($imgData));
-
-//make sure caching is turned on
-header('Cache-Control: public');
-
-// content type
+header("Cache-Control: public, max-age=".$cacheHeaderTime.", s-maxage=".$cacheHeaderTime."");
 header('Content-type: ' . $mimetype);
-
 echo $imgData;
 
 
 // Functions
-function cacheTile($tileUrl, $cacheTileFile) {
-  global $cacheFileTime;
-
-  if(file_exists($cacheTileFile)) {
-    logger($tileUrl." Tile exists");
-    $fileAge = time()-filemtime($cacheTileFile);
-    if($fileAge > $cacheFileTime) {
-      logger($tileUrl." cache old");
-      if($data = downloadTile($tileUrl)) {
-        logger($tileUrl." cache renewed");
-        saveTile($cacheTileFile, $data);
-        return $data;
-      }else{
-        logger($tileUrl." cache renew error");
-        return false;
-      }
-    }
-    logger($tileUrl." use cache");
-    return file_get_contents($cacheTileFile);
-  }else{
-    logger($tileUrl." Tile not cached");
-    if($data = downloadTile($tileUrl)) {
-      logger($tileUrl." cache created");
-      saveTile($cacheTileFile, $data);
-      return $data;
-    }else{
-      logger($tileUrl." cache create error");
-      return false;
-    }
-  }
-}
-
-function saveTile($cacheTileFile, $data) {
+function cacheTile($tileURL, $cacheTileFile) {
   if(!file_exists(dirname($cacheTileFile))) {
     mkdir(dirname($cacheTileFile), 0777, true);
   }
-  return file_put_contents($cacheTileFile, $data);
-}
 
-function downloadTile($tileUrl) {
-  $ch = curl_init($tileUrl);
+  $ch = curl_init($tileURL);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
 
   $data = curl_exec($ch);
   $header = curl_getinfo($ch);
 
   if( $header['http_code'] == "200"){
+    file_put_contents($cacheTileFile, $data);
     curl_close($ch);
-    return $data;
+    return true;    
   }else{
     curl_close($ch);
-    return false;
+    return false;     
   }
 }
 
